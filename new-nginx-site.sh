01ba868a2dfaa4f0c1f4e300e903c35f5b17717c
Slava Knyazev <slava@knyazev.io>
2018-09-07 22:23:34 -0400

    Added nginx validation

--- new-nginx-site.sh
+++ new-nginx-site.sh
@@ -3,21 +3,21 @@ PORT=$2
 
 if [ -z "$DOMAIN" ]; then echo "Please supply domain!
 Usage:
-	secureproxy.sh [domain] [port]
-"; exit 1; else echo "Generating NGINX Configuration with SSL certs for 
+	./new-nginx-site.sh [domain] [port]
+"; exit 1; else echo "Generating NGINX Configuration with SSL certs for
 $DOMAIN and www.$DOMAIN"; fi
 
 if [ -z "$PORT" ]; then echo "Please supply port number!
 Usage:
-        secureproxy.sh [domain] [port]
+        ./new-nginx-site.sh [domain] [port]
 "; exit 1; else echo "Proxying service from port $PORT"; fi
 
 echo "[+] Generate new certs"
 ./getcert.sh $DOMAIN
 
 if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
-	echo "File not found!"
-	exit 1
+    echo "[-] It appears letsencrypt failed to generate a cert. Exiting."
+    exit 1
 fi
 
 echo "[+] Create nginx conf file"
@@ -27,7 +27,7 @@ echo "server {
 	listen 443 ssl;
 	listen [::]:443 ssl;
 	server_name $DOMAIN www.$DOMAIN;
-	add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+	add_header Strict-Transport-Security \"max-age=31536000; includeSubDomains\" always;
 	ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
 	ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
 	location /.well-known {
@@ -42,6 +42,15 @@ echo "server {
 	}
 }
 " > /etc/nginx/sites-available/$DOMAIN.conf
+echo "[+] Running nginx configuration test"
+if nginx -t 2>/dev/null; then 
+	echo "[+] NGINX configuration passed."; 
+	rm /etc/nginx/sites-available/$DOMAIN.conf
+else
+	echo "[-] NGINX validation failed. Removing site.";
+	exit 1;
+fi
+
 echo "[+] Enable site"
 ln -s /etc/nginx/sites-available/$DOMAIN.conf /etc/nginx/sites-enabled/
 
